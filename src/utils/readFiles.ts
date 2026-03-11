9a9b6f50a3d1053ca6b369039f3282b45203366d
travisread33 <travis.read.33@gmail.com>
2026-03-11 13:17:47 -0600

    Update readFiles.ts
    Added error handling around reading a file

--- src/utils/readFiles.ts
+++ src/utils/readFiles.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
 
 export async function readInputsFile(filePath: string): Promise<string[]> {
-  let inputs = await fs.promises.readFile(filePath, "utf8");
-  return inputs.split("\n");
+  let inputs = "";
+  try {
+    inputs = await fs.promises.readFile(filePath, "utf8");
+    return inputs.split("\n");
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return [];
+  }
 }
